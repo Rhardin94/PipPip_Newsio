3daab33240966f83e4b01271e1c356aed4d59b8a
Robert H <robertjameshardin@aol.com>
2019-05-28 17:31:29 -0500

    scrape working correctly, fuctioning filter to remove existing headlines from scrape into new array to add to database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,14 +22,30 @@ module.exports = (app) => {
         if ((result.headline !== "") && (result.summary !== "") && (!result.url.includes("https://"))) {
         //Prepending first part of url to each result.url for functioning links  
           result.url = "https://www.bbc.com" + result.url;
-          resArray.push(result.headline);
+          resArray.push(result);
         }
       });
+      //Solution for separating stored scraped data found here: https://codeburst.io/javascript-array-distinct-5edc93501dc4
+      //Array for holding distinct scraped data
+      let distinctArray = [];
+      //Setting map constructor to a variable
+      const map = new Map();
+      //Looping through unfiltered array
+      for (const item of resArray) {
+        //Checking if map constructor has each headline
+        if (!map.has(item.headline)) {
+          //If not, adds to headline to the constructor for mapping through
+          map.set(item.headline, true);
+          //Pushes distanct data from map above into distinct array in JSON format once again
+          distinctArray.push({
+            headline: item.headline,
+            summary: item.summary,
+            url: item.url
+          });
+        }
+      }
       //Logging the results.
-      //console.log(resArray);
-      resArray.forEach((results) => {
-        console.log(results.headline);
-      });
+      console.log(distinctArray);
     });
     res.send("Scrape Complete!");
   });
